775d4b64d362254b59cce72156b118471ba55920
Hussa-boop <153098499+Hussa-boop@users.noreply.github.com>
2025-08-11 22:49:24 +0300

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -20,29 +20,29 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBHn3xh93bnOju9oAeYiQTEw7_hChrBCA0',
-    appId: '1:1094510394317:web:1eafc573936900fb356284',
-    messagingSenderId: '1094510394317',
-    projectId: 'shipment-6b76f',
-    authDomain: 'shipment-6b76f.firebaseapp.com',
-    storageBucket: 'shipment-6b76f.firebasestorage.app',
-    measurementId: 'G-2H6V0PWQGW',
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    authDomain: '',
+    storageBucket: '',
+    measurementId: '',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDb3g3OftVEVxXQ1dBwC5UP6R0Wqr2ay84',
-    appId: '1:1094510394317:android:585b9bfa0649a114356284',
-    messagingSenderId: '1094510394317',
-    projectId: 'shipment-6b76f',
-    storageBucket: 'shipment-6b76f.firebasestorage.app',
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    storageBucket: '',
   );
-
+//قم بوضع الapikey الذي تم اعطائك من firebase بعد إنشائك للمشروع في الموقع 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBHn3xh93bnOju9oAeYiQTEw7_hChrBCA0',
-    appId: '1:1094510394317:ios:2c83369b67f0bb26356284',
-    messagingSenderId: '1094510394317',
-    projectId: 'shipment-6b76f',
-    storageBucket: 'shipment-6b76f.firebasestorage.app',
-    iosBundleId: 'com.example.dashboard',
+    apiKey: '',
+    appId: '',
+    messagingSenderId: '',
+    projectId: '',
+    storageBucket: '',
+    iosBundleId: '',
   );
 }
